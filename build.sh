77254b573ada054d167c5308e6bddd7e7a4ad88b
Cannonbait <ivar_busig@hotmail.com>
2015-01-28 13:16:30 +0100

    Fixed classpath

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf lab1/src/*.class
-javac -cp .:../ lab1/src/*.java
-2 "tsim --speed 5 Map.map" "java -cp .:../:lab1/src/ Main 10 5"
+javac -cp . lab1/src/*.java
+2 "tsim --speed 5 Map.map" "java -cp .:lab1/src/ Main 10 5"
